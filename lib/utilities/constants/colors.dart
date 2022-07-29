bd3f2b01e8ccc314fb65c19ad518237836fcaaa8
Precious <olwafemiprecious2017@gmail.com>
2022-07-29 13:17:01 +0100

    style guide

---
+++ lib/utilities/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff1C77C3);
+const kAlertSuccessColor = Color(0xff309F30);
+const kAlertWarningColor = Color(0xffFFB800);
+const kAlertErrorColor = Color(0xffE72B1F);
+const kAlertInfoColor = Color(0xff2757FF);
+const kAlertDisabledColor = Color(0xffCCCCCC);
+const kDefaultButtonColor = Color(0xff428ECD);
+const kHoverButtonColor = Color(0xff1C77C3);
+const kDisabledButtonColor = Color(0xffB3B3B3);
---
+++ lib/utilities/constants/text_styles.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+const kHeading1TextStyle = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+);
+const kHeading2TextStyle = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+);
+const kHeading3TextStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+const kBody1TextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+);
+const kBody2TextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+const kBody3TextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+const kLargeBody1TextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+const kLargeBody2TextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+const kLargeBody3TextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+const kMediumBody1TextStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+);
+const kMediumBody2TextStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+const kMediumBody3TextStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+const kSmallBody1TextStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w700,
+);
+const kSmallBody2TextStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w600,
+);
+const kSmallBody3TextStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+);
